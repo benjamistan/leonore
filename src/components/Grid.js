1ea5de681bd4146f5c223a8fbe77cbd0912a3fa0
SureVX <ben@surevx.com>
2020-11-17 06:23:10 +0100

    minor updates

---
+++ src/components/Grid.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+
+class Grid extends Component {
+    getAddingSum() {
+        const rndNum = () => Math.floor(Math.random() * (5 - 1 + 1) + 1);
+        return `${rndNum()} + ${rndNum()} = `;
+    }
+
+    getSubtractingSum() {
+        const a = Math.floor(Math.random() * (5 - 0 + 1) + 0);
+        const b = Math.floor(Math.random() * (5 - 0 + 1) + 0);
+        if(a >= b) {
+            return `${a} - ${b} =`;
+        } else {
+            return `${b} - ${a} = `;
+        }
+    }
+
+    getRandomSum() {
+        const rnd = Math.random();
+        if (rnd > 0.5) { 
+            return this.getAddingSum();
+        } else {
+            return this.getSubtractingSum();
+        }
+    }
+    
+    render() {
+        return (
+            <div className='ui grid' style={{fontWeight: "bold", fontSize: "1.6em"}}>
+                <div className='row' style={{padding: "40px"}}>
+                    <div class="four wide column">{this.getRandomSum()}</div>
+                    <div class="four wide column">{this.getRandomSum()}</div>
+                    <div class="four wide column">{this.getRandomSum()}</div>
+                    <div class="four wide column">{this.getRandomSum()}</div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Grid;
