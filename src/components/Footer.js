84aa10cc58515ffffd0df667aedc1e5a8b7377e5
SureVX <ben@surevx.com>
2021-01-13 09:36:37 +0100

    test: testing AWS Code Commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer() {
 	return (
 		<div className='no-print' style={{ paddingTop: '70px' }}>
-			&#169; {new Date().getFullYear()} Ben Stanley
+			&#169; {new Date().getFullYear()} Benjamin Stanley
 		</div>
 	);
 }
